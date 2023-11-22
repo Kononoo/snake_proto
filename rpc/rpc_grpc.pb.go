9996c504b2be30d0a16704af881aa7e8baaf7cf1
gitlab ci <gitlab@example.com>
2023-11-22 13:21:22 +0000

    feat: protobuf [ci skip]

--- rpc/rpc_grpc.pb.go
+++ rpc/rpc_grpc.pb.go
@@ -325,3 +325,87 @@ var MusicServer_ServiceDesc = grpc.ServiceDesc{
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "rpc/rpc.proto",
 }
+
+// SnakeApiServerClient is the client API for SnakeApiServer service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type SnakeApiServerClient interface {
+	CommonReq(ctx context.Context, in *RpcReqParam, opts ...grpc.CallOption) (*RpcRspData, error)
+}
+
+type snakeApiServerClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewSnakeApiServerClient(cc grpc.ClientConnInterface) SnakeApiServerClient {
+	return &snakeApiServerClient{cc}
+}
+
+func (c *snakeApiServerClient) CommonReq(ctx context.Context, in *RpcReqParam, opts ...grpc.CallOption) (*RpcRspData, error) {
+	out := new(RpcRspData)
+	err := c.cc.Invoke(ctx, "/rpc.SnakeApiServer/commonReq", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// SnakeApiServerServer is the server API for SnakeApiServer service.
+// All implementations should embed UnimplementedSnakeApiServerServer
+// for forward compatibility
+type SnakeApiServerServer interface {
+	CommonReq(context.Context, *RpcReqParam) (*RpcRspData, error)
+}
+
+// UnimplementedSnakeApiServerServer should be embedded to have forward compatible implementations.
+type UnimplementedSnakeApiServerServer struct {
+}
+
+func (UnimplementedSnakeApiServerServer) CommonReq(context.Context, *RpcReqParam) (*RpcRspData, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CommonReq not implemented")
+}
+
+// UnsafeSnakeApiServerServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to SnakeApiServerServer will
+// result in compilation errors.
+type UnsafeSnakeApiServerServer interface {
+	mustEmbedUnimplementedSnakeApiServerServer()
+}
+
+func RegisterSnakeApiServerServer(s grpc.ServiceRegistrar, srv SnakeApiServerServer) {
+	s.RegisterService(&SnakeApiServer_ServiceDesc, srv)
+}
+
+func _SnakeApiServer_CommonReq_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(RpcReqParam)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SnakeApiServerServer).CommonReq(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/rpc.SnakeApiServer/commonReq",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SnakeApiServerServer).CommonReq(ctx, req.(*RpcReqParam))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// SnakeApiServer_ServiceDesc is the grpc.ServiceDesc for SnakeApiServer service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var SnakeApiServer_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "rpc.SnakeApiServer",
+	HandlerType: (*SnakeApiServerServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "commonReq",
+			Handler:    _SnakeApiServer_CommonReq_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "rpc/rpc.proto",
+}
