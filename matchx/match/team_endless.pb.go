f9e2e60b349f237bbce2e116a907e8f0d80b47ea
gitlab ci <gitlab@example.com>
2024-11-15 03:41:25 +0000

    feat: protobuf [ci skip]

--- matchx/match/team_endless.pb.go
+++ matchx/match/team_endless.pb.go
@@ -143,6 +143,53 @@ func (*TeamEndlessUploadAttr) Descriptor() ([]byte, []int) {
 	return file_matchx_match_team_endless_proto_rawDescGZIP(), []int{2}
 }
 
+type TeamEndlessRoomMember struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	GameCount int64 `protobuf:"varint,1,opt,name=game_count,json=gameCount,proto3" json:"game_count,omitempty"` // 游戏局数
+}
+
+func (x *TeamEndlessRoomMember) Reset() {
+	*x = TeamEndlessRoomMember{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_matchx_match_team_endless_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *TeamEndlessRoomMember) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*TeamEndlessRoomMember) ProtoMessage() {}
+
+func (x *TeamEndlessRoomMember) ProtoReflect() protoreflect.Message {
+	mi := &file_matchx_match_team_endless_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use TeamEndlessRoomMember.ProtoReflect.Descriptor instead.
+func (*TeamEndlessRoomMember) Descriptor() ([]byte, []int) {
+	return file_matchx_match_team_endless_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *TeamEndlessRoomMember) GetGameCount() int64 {
+	if x != nil {
+		return x.GameCount
+	}
+	return 0
+}
+
 var File_matchx_match_team_endless_proto protoreflect.FileDescriptor
 
 var file_matchx_match_team_endless_proto_rawDesc = []byte{
@@ -154,11 +201,14 @@ var file_matchx_match_team_endless_proto_rawDesc = []byte{
 	0x73, 0x4d, 0x61, 0x74, 0x63, 0x68, 0x53, 0x75, 0x63, 0x63, 0x65, 0x73, 0x73, 0x12, 0x12, 0x0a,
 	0x04, 0x68, 0x6f, 0x73, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x68, 0x6f, 0x73,
 	0x74, 0x22, 0x17, 0x0a, 0x15, 0x54, 0x65, 0x61, 0x6d, 0x45, 0x6e, 0x64, 0x6c, 0x65, 0x73, 0x73,
-	0x55, 0x70, 0x6c, 0x6f, 0x61, 0x64, 0x41, 0x74, 0x74, 0x72, 0x42, 0x2e, 0x5a, 0x2c, 0x67, 0x69,
-	0x74, 0x2e, 0x31, 0x37, 0x7a, 0x6a, 0x68, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x6e, 0x61, 0x6b,
-	0x65, 0x2f, 0x73, 0x6e, 0x61, 0x6b, 0x65, 0x5f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x6d, 0x61,
-	0x74, 0x63, 0x68, 0x78, 0x2f, 0x6d, 0x61, 0x74, 0x63, 0x68, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x33,
+	0x55, 0x70, 0x6c, 0x6f, 0x61, 0x64, 0x41, 0x74, 0x74, 0x72, 0x22, 0x36, 0x0a, 0x15, 0x54, 0x65,
+	0x61, 0x6d, 0x45, 0x6e, 0x64, 0x6c, 0x65, 0x73, 0x73, 0x52, 0x6f, 0x6f, 0x6d, 0x4d, 0x65, 0x6d,
+	0x62, 0x65, 0x72, 0x12, 0x1d, 0x0a, 0x0a, 0x67, 0x61, 0x6d, 0x65, 0x5f, 0x63, 0x6f, 0x75, 0x6e,
+	0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x09, 0x67, 0x61, 0x6d, 0x65, 0x43, 0x6f, 0x75,
+	0x6e, 0x74, 0x42, 0x2e, 0x5a, 0x2c, 0x67, 0x69, 0x74, 0x2e, 0x31, 0x37, 0x7a, 0x6a, 0x68, 0x2e,
+	0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x6e, 0x61, 0x6b, 0x65, 0x2f, 0x73, 0x6e, 0x61, 0x6b, 0x65, 0x5f,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x6d, 0x61, 0x74, 0x63, 0x68, 0x78, 0x2f, 0x6d, 0x61, 0x74,
+	0x63, 0x68, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -173,11 +223,12 @@ func file_matchx_match_team_endless_proto_rawDescGZIP() []byte {
 	return file_matchx_match_team_endless_proto_rawDescData
 }
 
-var file_matchx_match_team_endless_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_matchx_match_team_endless_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_matchx_match_team_endless_proto_goTypes = []interface{}{
 	(*TeamEndlessGroupMember)(nil),  // 0: match.TeamEndlessGroupMember
 	(*TeamEndlessMatchSuccess)(nil), // 1: match.TeamEndlessMatchSuccess
 	(*TeamEndlessUploadAttr)(nil),   // 2: match.TeamEndlessUploadAttr
+	(*TeamEndlessRoomMember)(nil),   // 3: match.TeamEndlessRoomMember
 }
 var file_matchx_match_team_endless_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -229,6 +280,18 @@ func file_matchx_match_team_endless_proto_init() {
 				return nil
 			}
 		}
+		file_matchx_match_team_endless_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*TeamEndlessRoomMember); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -236,7 +299,7 @@ func file_matchx_match_team_endless_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_matchx_match_team_endless_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
